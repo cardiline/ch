9cd9e6bead4de3c7f4db1b0844b753e381bf9918
cardiline <74353804+cardiline@users.noreply.github.com>
2020-11-13 13:15:36 +0200

    1

---
+++ js/translater.js
@@ -0,0 +1,269 @@
+var new_lang = {
+
+"lt0": "Besiegt die Hypertonie und<br> alle ihre Symptome",
+
+"lt1": "Normalisiert den Blutdruck auf natürliche Weise",
+
+"lt2": "Stellt die Elastizität der Gefäße wieder her",
+
+"lt3": "Senkt den Blutzuckerspiegel",
+
+"lt4": "Schützt vor Infarkten und Schlaganfällen",
+
+"lt5": "Mit Rabatt<br> bestellen",
+
+"lt6": "statt <span class=\"num price_old\"></span> ",
+
+"lt7": "SCHOCK",
+
+"lt8": "Die Herz-Kreislauferkrankungen sind die Hauptursache der Todesfälle weltweit. Den Daten der WHO zufolge, sind über 40% aller Todesfälle mit Erkrankungen des Herzens verbunden. <span>85% von diesen Todesfällen geschahen infolge eines Herzanfalls und eines Schlaganfalls.</span> ",
+
+"lt9": "Hypertonie ist eine ernsthafte Gefahr für das Leben und die Gesundheit!",
+
+"lt10": "Die Folgen des Bluthochdrucks führen zu:",
+
+"lt11": "Herzinsuffizienz",
+
+"lt12": "MYOKARDINFARKT",
+
+"lt13": "Hypertensive Krise",
+
+"lt14": "Schlaganfall",
+
+"lt15": "Koronare Herzkrankheit",
+
+"lt16": "Störung der Blutversorgung des Gehirns",
+
+"lt17": "Niereninsuffizienz",
+
+"lt18": "Diabetes Mellitus",
+
+"lt19": "Abschwächung und Verlust des Sehvermögens",
+
+"lt20": "Erektionsbeschwerden",
+
+"lt21": "Risikofaktoren",
+
+"lt22": "Alter",
+
+"lt23": "Menschen in einem Alter von über 35 Jahre sind der Entwicklung einer arteriellen Hypertension am häufigsten ausgesetzt.",
+
+"lt24": "genetische Veranlagung",
+
+"lt25": "Das Risiko der Entwicklung der arteriellen Hypertension steigt auf 80% an, wenn diese Erkrankung bei nächsten Verwandten vorhanden ist.",
+
+"lt26": "Stress",
+
+"lt27": "Die Stresshormone lösen eine Verengung der Gefäße und Arterien aus, indem sie das Herz zwingen, häufiger zu schlagen, infolge dessen der Blutdruck steigt.",
+
+"lt28": "Schlechte Gewohnheiten",
+
+"lt29": "Rauchen und Alkoholkonsum erhöhen den Blutdruck um ungefähr 10 mmHg pro Jahr",
+
+"lt30": "schlechte Ernährung",
+
+"lt31": " Überschüssiges Natrium in der Nahrung und eine Fülle von tierischen Fetten führen zu arteriellem Krampf und zur Entwicklung einer arteriellen Hypertonie.",
+
+"lt32": "Körperliche Inaktivität ", 
+
+"lt33": "Menschen, die sich nicht ausreichend bewegen, riskieren um 40%-50% mehr, an Hypertonie zu erkranken.",
+
+"lt34": "Übergewicht",
+
+"lt35": "Es ist bekannt, dass jedes Kilogramm des Übergewichts den Blutdruck um 2 mmHg steigert.",
+
+"lt36": "Schlafmangel",
+
+"lt37": "Die Bluthochdruckkranken können durch die Nichteinhaltung der Schlafnorm einen akuten Anstieg des Blutdrucks bekommen.",
+
+"lt38": "Die arterielle Hypertension verschleißt den gesamten Organismus<br> und führt zu dem Tod<span>Leiten Sie die Behandlung so schnell, wie möglich ein!</span> ",
+
+"lt39": "BEHANDLUNG EINLEITEN",
+
+"lt40": "3 Wirkungsstadien<img alt=\"\" src=\"img/logo.png\">",
+
+"lt41": "WIEDERHERSTELLUNG",
+
+"lt42": "Stabilisiert den arteriellen Blutdruck bis zu individuellen physiologischen Werten.",
+
+"lt43": "NORMALISIERUNG",
+
+"lt44": "Stellt den Herzrhythmus, die Zusammensetzung und die Zähigkeit des Blutes wieder her, vermindert die Hypertrophie des Herzens und der Gefäße.",
+
+"lt45": "SCHUTZ",
+
+"lt46": "Beugt atherosklerotische Veränderungen der Gefäße vor und schützt vor Infarkten und Schlaganfällen",
+
+"lt47": "Natürliche und leistungsstarke Rezeptur<img alt=\"\" src=\"img/logo.png\">",
+
+"lt48": "Brennnesselwurzel",
+
+"lt49": "Stellt die Nierenfunktion wieder her",
+
+"lt50": "Erhöht die Menge der Erythrozyten",
+
+"lt51": "Senkt den Blutzuckerspiegel",
+
+"lt52": "Baldrianwurzel",
+
+"lt53": "Beruhigt das zentrale Nervensystem",
+
+"lt54": "Senkt den arteriellen Blutdruck",
+
+"lt55": "Bringt die Funktion des Herz-Kreislaufsystems in die Norm",
+
+"lt56": "Thyamin",
+
+"lt57": "Normalisiert den Fett- und Kohlenhydrate Stoffwechsel",
+
+"lt58": "Verbessert die Funktion des Herzmuskels",
+
+"lt59": "Verlangsamt den Vorgang der Alterung der Gefäße",
+
+"lt60": "Adermin",
+
+"lt61": "Senkt den Tonus der Gefäße",
+
+"lt62": "Erhöht die Produktion von Serotonin, indem es die Stresses vorbeugt",
+
+"lt63": "Steigert die Belastbarkeit",
+
+"lt64": "Cobalamin",
+
+"lt65": "Bringt den Blutdruck in die Norm",
+
+"lt66": "Stellt Energievorräte in Zellen her",
+
+"lt67": "Trägt zur Bildung von Blutkörperchen bei",
+
+"lt68": "Riboflavin",
+
+"lt69": "Hilft dabei, Unruhe zu überwältigen",
+
+"lt70": "Beugt die Entwicklung von Anämie vor",
+
+"lt71": "Regt die Sättigung der Zellen mit Sauerstoff an",
+
+"lt72": "Weißdornenblätter",
+
+"lt73": "Verstärkt den Kreislauf in den Gefäßen",
+
+"lt74": "Verfügt über eine sedative Wirkung",
+
+"lt75": "Regt die Blutversorgung des Gehirns an",
+
+"lt76": "Ergebnisse der Studien",
+
+"lt77": "2018 hat das Nationale Institut der Herzgesundheit klinische Studien mit dem XX durchgeführt. An der Studie haben 221 Menschen in verschiedenem Alter teilgenommen, die an erhöhtem arteriellen Blutdruck leiden.",
+
+"lt78": " <span>98% der Probanden</span> - in den ersten Stunden nach der Einnahme, fand eine Stabilisierung des Bluthochdruck bei 130/80.",
+
+"lt79": " <span>97% der Probanden</span> - Sind alle Symptome der hypertensiven Krankheit nach den ersten zwei Wochen der kurförmigen Einnahme losgeworden",
+
+"lt80": " <span>82% der Probanden</span> - stellte fest, dass nach den ersten drei Tagen nach Einnahme des Arzneimittels keine Anfälle von Bluthochdruck auftraten",
+
+"lt81": "BEHANDLUNG EINLEITEN",
+
+"lt82": "Meinung des Facharztes",
+
+"lt83": "„Die Hypertension ist eine Krankheit unserer Zeit. Das ist ein äußerst gefährlicher Zustand, der jederzeit zu irreparablen Folgen führen kann. Deshalb ist es besonders wichtig, rechtzeitig die richtige Behandlung einzuleiten.",
+
+"lt84": "Die überwiegende Mehrheit der blutdrucksenkenden Arzneimitteln wirkt auf die Hypertension nur symptomatisch, indem sie die Notwendigkeit auslöst, die Arzneimittel stets einzunehmen. XX wirkt aber auf die Ursache der Krankheit, indem es die Gefäße festigt und ihre Elastizität steigert.",
+
+"lt85": "Es beginnt gleich nach der Einnahme schnell, zu wirken, indem es den Anfall beseitigt, und wirkt „aus der Distanz“, indem es die Zellen der Gefäße erneuert.",
+
+"lt86": "Dieses Mittel weist eine hohe Wirksamkeit bei jeder Form der Hypertonie auf“.",
+
+"lt87": "Dr. Ralf Bartels, Kardiologe",
+
+"lt88": "WAS WERDEN SIE NACH DER EINNAHMEKUR ERZIELEN<img alt=\"\" src=\"img/logo.png\">",
+
+"lt89": "Normaler Blutdruck",
+
+"lt90": "Gesunde Gefäße",
+
+"lt91": "Guter Schlaf",
+
+"lt92": "Starkes Abwehrsystem",
+
+"lt93": "Widerstandszunahme",
+
+"lt94": "es reicht mit dem Risiko! <span>Leiten Sie die Behandlung schnellstmöglich ein!</span> ",
+
+"lt95": "BEHANDLUNG EINLEITEN",
+
+"lt96": "Erfahrungen unserer Kunden",
+
+"lt97": "„Ein ausgezeichnetes Mittel. Im Laufe von fast 4 Jahren stieg mein Blutdruck an. Manchmal konnte man leben, und manchmal bekam man sehr starke Angst, dass irgendein Gefäß platzen wird. Natürlich untersuchte ich die Ärzte und habe verschiedene Pillen getrunken. Als ich aber aufhörte, sie zu nehmen, kam alles wieder zurück. Nachdem ich begonnen habe, XX einzunehmen, hat der Blutdruck komplett aufgehört, mir Sorgen zu machen. Die Kur habe ich im Frühling gemacht, während des ganzen Sommers und im Herbst ist der Blutdruck nicht einmal gestiegen“.",
+
+"lt98": "Johanna, 42 Jahre",
+
+"lt99": "„Mein Blutdruck hat sich mehrmals am Tag verändert - ich wusste nicht einmal mehr, womit ich es stabilisieren kann. Habe wohl das ganze Sortiment der Mittel aus der Apotheke ausprobiert. Endlich habe ich XX für mich entdeckt, ich empfinde keinerlei Blutdruckanstiege mehr. Endlich konnte ich ruhig ausatmen und beginnen, vollwertig zu leben“.",
+
+"lt100": "Frank, 57 Jahre",
+
+"lt101": "„Ab 30 Jahren litt ich an Übergewicht, danach wurde darüber hinaus noch die Diabetes entdeckt, und ihr folgte dann die Hypertonie. Das alles ist bei mir erblich bedingt, deshalb habe ich mich mit meiner Krankheit abgefunden. Ich habe nicht mehr gelebt, sondern existierte nur noch. Die Tabletten habe ich löffelweise eingenommen — ich war vollständig von diesen abhängig. Auf Empfehlung einer Freundin, die früher an Bluthochdruck litt, habe ich eine Kur von XX gemacht. Seitdem begann ein neuer Abschnitt meines Lebens! Die Probleme mit dem Blutdruck sind nun weg, ich bin nun ein absolut gesunder Mensch. Allen, die einem solchen Unheil begegnet sind, empfehle ich dieses Mittel und wünsche die schnellstmögliche Genesung“.",
+
+"lt102": "Jana, 55 Jahre",
+
+"lt103": "„Ein ausgezeichnetes Mittel. Im Laufe von fast 4 Jahren stieg mein Blutdruck an. Manchmal konnte man leben, und manchmal bekam man sehr starke Angst, dass irgendein Gefäß platzen wird. Natürlich untersuchte ich die Ärzte und habe verschiedene Pillen getrunken. Als ich aber aufhörte, sie zu nehmen, kam alles wieder zurück. Nachdem ich begonnen habe, XX einzunehmen, hat der Blutdruck komplett aufgehört, mir Sorgen zu machen. Die Kur habe ich im Frühling gemacht, während des ganzen Sommers und im Herbst ist der Blutdruck nicht einmal gestiegen“.",
+
+"lt104": "Johanna, 42 Jahre",
+
+"lt105": "„Mein Blutdruck hat sich mehrmals am Tag verändert - ich wusste nicht einmal mehr, womit ich es stabilisieren kann. Habe wohl das ganze Sortiment der Mittel aus der Apotheke ausprobiert. Endlich habe ich XX für mich entdeckt, ich empfinde keinerlei Blutdruckanstiege mehr. Endlich konnte ich ruhig ausatmen und beginnen, vollwertig zu leben“.",
+
+"lt106": "Frank, 57 Jahre",
+
+"lt107": "„Ab 30 Jahren litt ich an Übergewicht, danach wurde darüber hinaus noch die Diabetes entdeckt, und ihr folgte dann die Hypertonie. Das alles ist bei mir erblich bedingt, deshalb habe ich mich mit meiner Krankheit abgefunden. Ich habe nicht mehr gelebt, sondern existierte nur noch. Die Tabletten habe ich löffelweise eingenommen — ich war vollständig von diesen abhängig. Auf Empfehlung einer Freundin, die früher an Bluthochdruck litt, habe ich eine Kur von XX gemacht. Seitdem begann ein neuer Abschnitt meines Lebens! Die Probleme mit dem Blutdruck sind nun weg, ich bin nun ein absolut gesunder Mensch. Allen, die einem solchen Unheil begegnet sind, empfehle ich dieses Mittel und wünsche die schnellstmögliche Genesung“.",
+
+"lt108": "Jana, 55 Jahre",
+
+"lt109": "„Ein ausgezeichnetes Mittel. Im Laufe von fast 4 Jahren stieg mein Blutdruck an. Manchmal konnte man leben, und manchmal bekam man sehr starke Angst, dass irgendein Gefäß platzen wird. Natürlich untersuchte ich die Ärzte und habe verschiedene Pillen getrunken. Als ich aber aufhörte, sie zu nehmen, kam alles wieder zurück. Nachdem ich begonnen habe, XX einzunehmen, hat der Blutdruck komplett aufgehört, mir Sorgen zu machen. Die Kur habe ich im Frühling gemacht, während des ganzen Sommers und im Herbst ist der Blutdruck nicht einmal gestiegen“.",
+
+"lt110": "Johanna, 42 Jahre",
+
+"lt113": "Gebrauchshinweise",
+
+"lt114": "1 Kapsel morgens",
+
+"lt115": "Kapseln vor der Nahrungsaufnahme einnehmen",
+
+"lt116": "1 Kapsel morgens",
+
+"lt117": "Kapseln vor der Nahrungsaufnahme einnehmen",
+
+"lt118": "Das Ergebnis der kurförmigen Einnahme ist eine nachhaltige Remission im Laufe eines langen Zeitraums!",
+
+"lt119": "Besiegt die Hypertonie und<br> alle ihre Symptome",
+
+"lt120": "Normalisiert den Blutdruck auf einem natürlichen Wege",
+
+"lt121": "Stellt die Elastizität der Gefäße wieder her",
+
+"lt122": "Senkt den Blutzuckerspiegel",
+
+"lt123": "Schützt vor Infarkten und Schlaganfällen",
+
+"lt124": "Mit Rabatt<br> bestellen",
+
+"lt125": "statt <span class=\"num price_old\"></span> ",
+
+"lt126": "Wie wir arbeiten",
+
+"lt127": "Hinterlassen Sie eine Anfrage auf unserer Webseite",
+
+"lt128": "Die Fachkraft wird Kontakt mit Ihnen zur Bestätigung der Bestellung aufnehmen",
+
+"lt129": "Bezahlung bei dem Empfang der Bestellung",
+
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
